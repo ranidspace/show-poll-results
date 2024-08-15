6086bee4784c26cea7b0f9799b8dfc787779e715
ranidspace <ranidspace@proton.me>
2024-08-15 00:04:00 -0400

    remove and rename files

--- unfucker.js
+++
@@ -1,1 +0,0 @@
-//filename was changed: go to https://github.com/enchanted-sword/dashboard-unfucker, select the version you want, and hit the "raw" button with tampermonkey installed to install the script
